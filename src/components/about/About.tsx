7d7fb8849ce9f0b6e032a7e9e82d31a2ab422f91
harry-fruit <isaqueduarte17@gmail.com>
2024-06-14 14:49:21 -0300

    improv: finalizar estrutra de section about com mediaquery

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -3,28 +3,30 @@ import style from "@/styles/about.module.css";
 
 export const About: React.FC = (): JSX.Element => {
   return (
-    <section id={"about"} className={`w-full p-2 md:py-4 md:px-32 ${style.aboutContainer}`}>
-      <div id="header-wrapper" className={`${style.header} `}>
-        <h2>Hey, Im Isaque.</h2>
-        <h4>
+    <section id={"about"} className={`w-full py-10 px-2 md:px-6 xl:px-14 2xl:px-32 ${style.aboutContainer}`}>
+      <div id="header-wrapper" className={`${style.header} flex flex-col justify-center items-center relative`}>
+        <h2 className="text-3xl w-full text-center md:text-start">Hey, Im Isaque.</h2>
+        <h4 className={"text-xl py-4 text-center md:text-start"}>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae
           temporibus quam dolores labore laborum, eius iste voluptatem, quas,
           illum rerum ex vero consequatur?
         </h4>
+        <div className={`${style.lineSplit} absolute bottom-0 md:left-0`}/>
       </div>
-      <div id="text-wrapper" className={`${style.txt}`}>
-        <p>
+      <div id="text-wrapper" className={`${style.txt} flex flex-col justify-center gap-2 md:mt-4`}>
+        <p className="text-center md:text-start">
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempora
           quisquam voluptates, nisi, debitis ipsum voluptatum possimus eius qui
           temporibus unde nobis, voluptas officiis aspernatur modi accusantium?
           Ab expedita obcaecati quam.
         </p>
-        <p>
+        <p className="text-center md:text-start">
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempora
           quisquam voluptates, nisi, debitis ipsum voluptatum possimus eius qui
           temporibus unde nobis, voluptas officiis aspernatur modi accusantium?
           Ab expedita obcaecati quam.
         </p>
+        <p className="text-center md:text-start">Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, quasi vitae perferendis commodi placeat molestiae sapiente delectus.</p>
       </div>
       <div id="imageWrapper" className={`w-full h-full relative ${style.picture} flex flex-col items-center justify-center`}>
         <Image
@@ -32,7 +34,7 @@ export const About: React.FC = (): JSX.Element => {
           alt="Picture from Isaque Duarte"
           height={300}
           width={300}
-          className="rounded-full object-cover w-[350px] h-[350px]"
+          className="rounded-full object-cover w-[300px] h-[300px] lg:w-[350px] lg:h-[350px]"
         />
       </div>
     </section>
