745bc2a7cfca0396fd936748cb991bab3b9be179
harry-fruit <isaqueduarte17@gmail.com>
2024-06-15 14:03:08 -0300

    improv: adicionar cor primaria de bg no tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,19 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      backgroundColor: {
+        primary: {
+          100: "#cfebff",
+          200: "#9fd7ff",
+          300: "#70c2ff",
+          400: "#40aeff",
+          500: "#109aff",
+          600: "#0d7bcc",
+          700: "#0a5c99",
+          800: "#063e66",
+          900: "#031f33"
+},
+      },
       blur: {
         "soft": ".5px"
       }
