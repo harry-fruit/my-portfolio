aa984dd2f179a10c2cb3a01fcbc5353e9793df3c
harry-fruit <isaqueduarte17@gmail.com>
2024-06-16 21:14:51 -0300

    improv: limpar código

--- src/components/ResponsiveSlideGrid.tsx
+++ src/components/ResponsiveSlideGrid.tsx
@@ -1,13 +1,13 @@
 "use client";
 
-// import useSize from "@/hooks/useSize";
+import useSize from "@/hooks/useSize";
 import React, { useRef, useState } from "react";
 
 type Props = {
   children: React.ReactNode;
 };
 
-export const ResponsiveSlideGrid: React.FC<Props> = ({ children }: Props) => {
+export const SlideComponent: React.FC<Props> = ({ children }: Props) => {
   const [currentSlide, setCurrentSlide] = useState<number>(0);
   const touchStartX = useRef(0);
   const touchEndX = useRef(0);
@@ -39,7 +39,7 @@ export const ResponsiveSlideGrid: React.FC<Props> = ({ children }: Props) => {
       prevSlide();
     }
   };
-  
+
   return (
     <div className="h-full w-full">
       <button onClick={nextSlide}>next</button>
@@ -51,7 +51,7 @@ export const ResponsiveSlideGrid: React.FC<Props> = ({ children }: Props) => {
         onTouchMove={handleTouchMove}
         onTouchEnd={handleTouchEnd}
       >
-        <div className="absolute left-0 overflow-scroll bg-white flex flex-nowrap">
+        <div className="absolute left-0 overflow-scroll bg-white flex flex-nowrap lg:w-0">
           {children}
         </div>
       </div>
@@ -59,31 +59,21 @@ export const ResponsiveSlideGrid: React.FC<Props> = ({ children }: Props) => {
   );
 };
 
-// return (
-//     <div>
-//       {isMobile ? (
-//         <div className="relative">
-//           <div className="overflow-hidden">
-//             <div
-//               className="whitespace-nowrap transition-transform duration-300"
-//               style={{ transform: `translateX(-${currentSlide * 100}%)` }}
-//             >
-//               {React.Children.map(children, (child) => (
-//                 <div className="inline-block w-full">{child}</div>
-//               ))}
-//             </div>
-//           </div>
-//           <button
-//             onClick={nextSlide}
-//             className="absolute top-1/2 right-0 transform -translate-y-1/2 bg-blue-500 text-white px-4 py-2"
-//           >
-//             Next
-//           </button>
-//         </div>
-//       ) : (
-//         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-//           {children}
-//         </div>
-//       )}
-//     </div>
-//   );
+export const ResponsiveSlideGrid: React.FC<Props> = ({ children }: Props) => {
+  const { width } = useSize();
+  const isSmallDevice = (width || 0) < 1024;
+
+  return isSmallDevice ? (
+    <SlideComponent>{children}</SlideComponent>
+  ) : (
+    <GridComponent>{children}</GridComponent>
+  );
+};
+
+export const GridComponent: React.FC<Props> = ({ children }: Props) => {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      {children}
+    </div>
+  );
+};
