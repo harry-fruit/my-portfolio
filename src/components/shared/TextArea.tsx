bb1ddff0ea82cdacf2d7b8d0f01d3d7015b124dc
harry-fruit <isaqueduarte17@gmail.com>
2024-06-18 15:59:22 -0300

    feat: add textarea component

---
+++ src/components/shared/TextArea.tsx
@@ -0,0 +1,44 @@
+import { ValidationError } from "@formspree/react";
+import {
+  FieldValues,
+  SubmissionError,
+  SubmissionSuccess,
+} from "@formspree/core";
+import style from "@/styles/input.module.scss";
+
+type StateProps = {
+  errors: SubmissionError<FieldValues> | null;
+  result: SubmissionSuccess | null;
+  submitting: boolean;
+  succeeded: boolean;
+};
+type Props = {
+id: string;
+  required?: boolean;
+  className?: string;
+  name: string;
+  state: StateProps;
+  placeholder: string;
+};
+
+export const TextArea = ({
+  required = false,
+  className = "",
+  name,
+  state,
+  placeholder,
+}: Props) => {
+  return (
+    <div className={`${style.formControl} ${className}`}>
+      <textarea
+        required={required}
+        placeholder={placeholder}
+        className={`${style.input} ${style.inputAlt}`}
+        name={name}
+        
+      />
+      <ValidationError prefix={"Message"} field={name} errors={state.errors} />
+      <span className={`${style.inputBorder} ${style.inputBorderAlt}`}></span>
+    </div>
+  );
+};
