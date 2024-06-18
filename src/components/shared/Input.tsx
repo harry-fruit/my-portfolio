cba00d9009a4fd421419b76ec7075330b8d38fa4
harry-fruit <isaqueduarte17@gmail.com>
2024-06-18 15:58:26 -0300

    feat: adicionando componente de input customizado

---
+++ src/components/shared/Input.tsx
@@ -0,0 +1,47 @@
+import style from "@/styles/input.module.scss";
+import { ValidationError } from "@formspree/react";
+import {
+  FieldValues,
+  SubmissionError,
+  SubmissionSuccess,
+} from "@formspree/core";
+
+type StateProps = {
+  errors: SubmissionError<FieldValues> | null;
+  result: SubmissionSuccess | null;
+  submitting: boolean;
+  succeeded: boolean;
+};
+
+type Props = {
+  id: string;
+  type: "text" | "email";
+  required?: boolean;
+  className?: string;
+  name: string;
+  state: StateProps;
+  placeholder: string;
+};
+
+export const Input = ({
+  type = "text",
+  required = false,
+  className = "",
+  name,
+  state,
+  placeholder,
+}: Props) => {
+  return (
+    <div className={`${style.formControl} ${className}`}>
+      <input
+        type={type}
+        required={required}
+        placeholder={placeholder}
+        className={`${style.input} ${style.inputAlt}`}
+        name={name}
+      />
+      <ValidationError prefix={type} field={name} errors={state.errors} />
+      <span className={`${style.inputBorder} ${style.inputBorderAlt}`}></span>
+    </div>
+  );
+};
