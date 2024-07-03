486254ddf906db79ade66b6d6ed9ed720733ecf2
Harry <isaqueduarte17@gmail.com>
2024-07-02 21:52:15 -0300

    remover axios

--- src/app/api/[locale]/generate-pdf/route.ts
+++ src/app/api/[locale]/generate-pdf/route.ts
@@ -1,19 +1,15 @@
 import { getAuthorizationHeader } from "@/util/authorization";
-import axios from "axios";
-import https from "https";
 
 export async function GET(request: Request, { params }: { params: { locale: string } }) {
   const locale = params.locale;
 
-  const headers = {
-    "Content-Type": "application/json",
-    "Authorization-Token": getAuthorizationHeader(),
-  };
-
-  const agent = new https.Agent({
-    rejectUnauthorized: false,
+  return await fetch(`${process.env.API_URL}/generate-pdf`, {
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization-Token": getAuthorizationHeader(),
+    },
+    body: JSON.stringify({ locale }),
+    method: "POST",
   });
-
-  return await axios.post(`${process.env.API_URL}/generate-pdf`, { locale }, { httpsAgent: agent, headers });
 
 }
