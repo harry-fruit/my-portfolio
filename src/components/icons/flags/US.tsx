5a262b35e330d674d4c5d5f18527ea15c167b930
Harry <isaqueduarte17@gmail.com>
2024-07-09 01:17:34 -0300

    fixs: svg properties

--- src/components/icons/flags/US.tsx
+++ src/components/icons/flags/US.tsx
@@ -21,7 +21,7 @@ export const USFlag = ({ color, width, height, className, onClick }: Props) => {
       <path fill="#bd3d44" d="M0 0h640v480H0" />
       <path
         stroke="#fff"
-        stroke-width="37"
+        strokeWidth="37"
         d="M0 55.3h640M0 129h640M0 203h640M0 277h640M0 351h640M0 425h640"
       />
       <path fill="#192f5d" d="M0 0h364.8v258.5H0" />
@@ -30,7 +30,7 @@ export const USFlag = ({ color, width, height, className, onClick }: Props) => {
       </marker>
       <path
         fill="none"
-        marker-mid="url(#us-a)"
+        markerMid="url(#us-a)"
         d="m0 0 16 11h61 61 61 61 60L47 37h61 61 60 61L16 63h61 61 61 61 60L47 89h61 61 60 61L16 115h61 61 61 61 60L47 141h61 61 60 61L16 166h61 61 61 61 60L47 192h61 61 60 61L16 218h61 61 61 61 60z"
       />
     </svg>
