a1ff035f5271301b8d202e53d48c4bf3150a988c
Harry <isaqueduarte17@gmail.com>
2024-07-10 12:27:00 -0300

    fix: remove ununsed layout

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,9 +1,18 @@
+import "@/styles/globals.scss";
+import { Metadata } from "next";
 import { NextIntlClientProvider } from "next-intl";
 import { getMessages } from "next-intl/server";
+import { ThemeProvider } from "next-themes";
+import { Toaster } from "react-hot-toast";
 
 type LocaleLayoutProps = {
   children: React.ReactNode;
   params: { locale: string };
+};
+
+export const metadata: Metadata = {
+  title: "Isaque Duarte | Portfolio",
+  description: "My Portfolio",
 };
 
 export default async function LocaleLayout({
@@ -13,11 +22,14 @@ export default async function LocaleLayout({
   const messages = await getMessages();
 
   return (
-    <html lang={locale}>
-      <body>
-        <NextIntlClientProvider messages={messages}>
-          {children}
-        </NextIntlClientProvider>
+    <html lang={locale} suppressHydrationWarning>
+      <body className="flex flex-col justify-center items-center bg-slate-100 dark:bg-slate-900/95">
+        <Toaster position="bottom-center" />
+        <ThemeProvider attribute="class">
+          <NextIntlClientProvider messages={messages}>
+            {children}
+          </NextIntlClientProvider>
+        </ThemeProvider>
       </body>
     </html>
   );
