601d66454a00ccb8ed4fc7abd0e765920bdb6cf4
Harry <isaqueduarte17@gmail.com>
2024-07-24 19:27:31 -0300

    improv: improve project folder structure

---
+++ src/app/(routes)/[locale]/layout.tsx
@@ -0,0 +1,36 @@
+import "@/styles/globals.scss";
+import { Metadata } from "next";
+import { NextIntlClientProvider } from "next-intl";
+import { getMessages } from "next-intl/server";
+import { ThemeProvider } from "next-themes";
+import { Toaster } from "react-hot-toast";
+
+type AppLayoutProps = {
+  children: React.ReactNode;
+  params: { locale: string };
+};
+
+export const metadata: Metadata = {
+  title: "Isaque Duarte | Portfolio",
+  description: "My Portfolio",
+};
+
+export default async function AppLayout({
+  children,
+  params: { locale },
+}: AppLayoutProps) {
+  const messages = await getMessages();
+
+  return (
+    <html lang={locale} suppressHydrationWarning>
+      <body className="flex flex-col justify-center items-center bg-slate-100 dark:bg-slate-900/95 relative">
+        <Toaster position="bottom-center" />
+        <ThemeProvider attribute="class">
+          <NextIntlClientProvider messages={messages}>
+            {children}
+          </NextIntlClientProvider>
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
